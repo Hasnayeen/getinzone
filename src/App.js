9f0d69dc3107689dfa17ecc859dffb1027e33954
Hasnayeen <searching.nehal@gmail.com>
2018-12-31 17:10:55 +0600

    Remove login & add timer completion bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import Timer from './Timer.jsx';
-import Navbar from './Navbar.jsx';
+import React, { Component } from 'react'
+import Timer from './Timer.jsx'
+import Navbar from './Navbar.jsx'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <Navbar />
-        <div className="container mx-auto px-4 flex flex-col flex-wrap items-center">
+        <div className='container mx-auto px-4 flex flex-col flex-wrap items-center'>
           <Timer />
-          <div className=" mt-16 p-4 text-grey-darker rounded">
-            <div className="text-grey-dark text-center pt-2 pb-4">Shourtcut Keys</div>
+          <div className=' mt-16 p-4 text-grey-darker rounded'>
+            <div className='text-grey-dark text-center pt-2 pb-4'>Shourtcut Keys</div>
             <div>
-              <span className="font-bold">S</span> : start the timer,
-              <span className="font-bold"> P</span> : pause the timer,
-              <span className="font-bold"> R</span> : reset the timer,
-              <span className="font-bold"> B</span> : switch to break,
-              <span className="font-bold"> C</span> : open/close config option
+              <span className='font-bold'>S</span> : start the timer,
+              <span className='font-bold'> P</span> : pause the timer,
+              <span className='font-bold'> R</span> : reset the timer,
+              <span className='font-bold'> B</span> : switch to break,
+              <span className='font-bold'> C</span> : open/close config option
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
